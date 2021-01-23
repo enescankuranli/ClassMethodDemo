7181e46e8792e01963571760ffbf127d8bfa0bf3
example <69421215+nefezann@users.noreply.github.com>
2021-01-23 14:38:33 +0300

    Proje dosyası ekle.

---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 01;
+            musteri1.Ad = "Enes Can";
+            musteri1.Soyad = "Kuranlı";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 02;
+            musteri2.Ad = "Buğra";
+            musteri2.Soyad = "Demirer";
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 03;
+            musteri3.Ad = "İrem";
+            musteri3.Soyad = "Yörük";
+
+            
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (var Musteri in musteriler)
+            {
+                Console.WriteLine(Musteri.Id + " " + Musteri.Ad + " " + Musteri.Soyad);
+            }
+            Console.WriteLine("****************Listelenenler*****************");
+            musterimanager musteri = new musterimanager();
+            musteri.Ekle(musteri1);
+            musteri.Ekle(musteri2);
+            musteri.Ekle(musteri3);
+            
+
+            Console.WriteLine("****************Silinenler***************");
+            
+
+
+
+        }
+    }
+}
---
+++ ClassMethodDemo/musterimanager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class musterimanager
+    {
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + "  " + "Eklenmiştir");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.Soyad + " " + "Silinmiştir");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler Listelendi");
+
+
+
+
+
+
+
+
+        }
+    }
+}
